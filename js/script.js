5b34c44e582de16854ab7b9eb58c2ca19fc9e817
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-11-02 15:04:49 +0100

    add scaffold

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+
+//   const container = document.querySelector('.container');
+ 
+// const bottone = document.querySelector('button');
+// const inputTag = document.getElementById('numero');
+// const testo = document.querySelector('h1');
+
+// bottone.addEventListener('click', function(){
+//     const num = parseInt(inputTag.value);
+    
+//    if(isNaN(num)){
+//     testo.innerHTML = '';
+//     const divAlert = notificationError('Devi inserire un  umero !');
+//     container.prepend(divAlert);
+//     return;
+//    }
+//    reFmoveFirstNotification();
+//    if(isEven(num)){
+//        testo.innerHTML = 'Il numero è pari !';
+//    } else {
+//         testo.innerHTML = 'Il numero è dispari !';
+//    }
+// })
+
